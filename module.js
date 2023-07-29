3dca430ce31694176a4ae24ff74debd4cd352f64
Esteban Garcia <estebangabrielgarciasanchez@gmail.com>
2023-07-28 22:38:31 -0300

    feat: tiny slider - second and third section - footer

--- module.js
+++ module.js
@@ -1,10 +1,12 @@
 import handleNavClick, { searchPage } from "./acc/functions.js";
+import tns from "./acc/tiny-slider.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   //Init page.js - routing
   const page = window.page;
+
   page("/", () => searchPage("/templates/home.html"));
   page("/dental", () => searchPage("/templates/dental.html"));
   page("/lab-dental", () => searchPage("/templates/lab-dental.html"));
@@ -16,4 +18,28 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
   //Init nav handler
   handleNavClick("navbar-burger-btn", "navbarClinic", "navbar-item");
+
+  //Init slider
+  tns({
+    container: ".my-slider",
+    loop: true,
+    items: 1,
+    slideBy: "page",
+    nav: false,
+    autoplay: true,
+    speed: 400,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: true,
+    controlsContainer: "#customize-controls",
+    responsive: {
+      640: {
+        items: 2,
+      },
+
+      768: {
+        items: 3,
+      },
+    },
+  });
 });
