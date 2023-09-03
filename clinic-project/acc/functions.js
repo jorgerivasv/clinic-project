6f68c843b1883b37cf328042db7549f4db6255f2
Esteban Garcia <estebangabrielgarciasanchez@gmail.com>
2023-09-03 16:02:46 -0300

    feat: lab dental

--- clinic-project/acc/functions.js
+++ clinic-project/acc/functions.js
@@ -26,6 +26,36 @@ export default function handleNavClick(buttonId, panelId, navItemsClass) {
   });
 }
 
+export function tabHandler() {
+  // Obtener todas las pestañas y sus contenidos
+  const tabs = document.querySelectorAll(".tabs li");
+  const tabContents = document.querySelectorAll(".tabs-content > div");
+
+  // Agregar un evento clic a cada pestaña
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      // Desactivar todas las pestañas y ocultar sus contenidos
+      tabs.forEach((t) => t.classList.remove("is-active"));
+      tabContents.forEach((content) =>
+        content.classList.remove("active-content")
+      );
+
+      // Activar la pestaña actual
+      tab.classList.add("is-active");
+
+      // Mostrar el contenido de la pestaña correspondiente
+      const tabId = tab.getAttribute("data-tab");
+      const tabContent = document.getElementById(tabId);
+      if (tabContent) {
+        tabContent.classList.add("active-content");
+      }
+    });
+  });
+}
+
 export function searchPage(link) {
   fetch(link)
     .then((response) => response.text())
@@ -35,6 +65,41 @@ export function searchPage(link) {
       const $bodyChildren = doc.querySelector("body");
       document.getElementById("content").innerHTML = $bodyChildren.innerHTML;
       if (link === "/templates/home.html") {
+        tns({
+          mode: "carousel", // or 'gallery'
+          mouseDrag: true,
+          navPosition: "bottom",
+          autoplay: true,
+          autoplayButtonOutput: false,
+          loop: true,
+          gutter: 0,
+          controlsContainer: "#custom_controlsContainer",
+          nextButton: "#prev",
+          nextButton: "#next", // String selector
+          arrowKeys: true, // keyboard support
+          lazyload: false,
+          lazyloadSelector: ".tns-lazy",
+          speed: 700,
+          startIndex: 0,
+          responsive: {
+            0: {
+              items: 1,
+              edgePadding: 30,
+              nav: false,
+            },
+            768: {
+              items: 3,
+              edgePadding: 0,
+            },
+            1024: {
+              items: 3,
+              edgePadding: 0,
+            },
+          },
+        });
+      }
+      if (link === "/templates/lab-dental.html") {
+        tabHandler();
         tns({
           mode: "carousel", // or 'gallery'
           mouseDrag: true,
