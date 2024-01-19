3bc92468c585df374fb9c36eda9e7017ba6cf6e0
Esteban Garcia <estebangabrielgarciasanchez@gmail.com>
2024-01-18 22:39:06 -0300

    fix: gmaps env

--- clinic-project/server.js
+++ clinic-project/server.js
@@ -54,7 +54,7 @@ const server = http.createServer((req, res) => {
   //Send GMAPS URL
   if (req.url === "/api/gmaps-url" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ href: `${process.env.GMAP_HREF}` }));
+    res.end(JSON.stringify({ href: process.env.GMAP_HREF }));
     return;
   }
 
